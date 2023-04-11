6c5f2f938cda95138feca182d50d790ac7a61a1d
ilhamibnu <e41210178@student.polije.ac.id>
2023-04-11 10:49:52 +0700

    templating admin dashboard

---
+++ resources/views/layouts/mainlayout.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Bumil App - @yield('title')</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <!-- Pignose Calender -->
+    <link href="./plugins/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
+    <!-- Chartist -->
+    <link rel="stylesheet" href="./plugins/chartist/css/chartist.min.css">
+    <link rel="stylesheet" href="./plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css">
+    <link href="./plugins/tables/css/datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="./plugins/sweetalert/css/sweetalert.css" rel="stylesheet">
+    <!-- Custom Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<!--*******************
+        Preloader start
+    ********************-->
+<div id="preloader">
+    <div class="loader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3"
+                stroke-miterlimit="10" />
+        </svg>
+    </div>
+</div>
+<!--*******************
+        Preloader end
+    ********************-->
+
+<div id="main-wrapper">
+
+    @include('partials.header')
+    @include('partials.navbar')
+    @include('partials.sidebar')
+
+</div>
+
+
+<!--**********************************
+Scripts
+***********************************-->
+<script src="plugins/common/common.min.js"></script>
+<script src="js/custom.min.js"></script>
+<script src="js/settings.js"></script>
+<script src="js/gleek.js"></script>
+<script src="js/styleSwitcher.js"></script>
+
+<!-- tables -->
+<script src="./plugins/tables/js/jquery.dataTables.min.js"></script>
+<script src="./plugins/tables/js/datatable/dataTables.bootstrap4.min.js"></script>
+<script src="./plugins/tables/js/datatable-init/datatable-basic.min.js"></script>
+
+<!-- Chartjs -->
+<script src="./plugins/chart.js/Chart.bundle.min.js"></script>
+<!-- Circle progress -->
+<script src="./plugins/circle-progress/circle-progress.min.js"></script>
+<!-- Datamap -->
+<script src="./plugins/d3v3/index.js"></script>
+<script src="./plugins/topojson/topojson.min.js"></script>
+<script src="./plugins/datamaps/datamaps.world.min.js"></script>
+<!-- Morrisjs -->
+<script src="./plugins/raphael/raphael.min.js"></script>
+<script src="./plugins/morris/morris.min.js"></script>
+<!-- Pignose Calender -->
+<script src="./plugins/moment/moment.min.js"></script>
+<script src="./plugins/pg-calendar/js/pignose.calendar.min.js"></script>
+<!-- ChartistJS -->
+<script src="./plugins/chartist/js/chartist.min.js"></script>
+<script src="./plugins/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js"></script>
+
+{{-- sweet alert --}}
+
+<script src="./plugins/sweetalert/js/sweetalert.min.js"></script>
+<script src="./plugins/sweetalert/js/sweetalert.init.js"></script>
+
+
+<script src="./js/dashboard/dashboard-1.js"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
+});
+
+Route::get('/index', function () {
+    return view('pages.index');
+});
+
+Route::get('/datauser', function () {
+    return view('pages.datauser');
+});
+
+Route::get('/datapengingat', function () {
+    return view('pages.datapengingat');
+});
+
+Route::get('/datapoli', function () {
+    return view('pages.datapoli');
+});
+
+Route::get('/pengingat', function () {
+    return view('pages.pengingat');
 });
